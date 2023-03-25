0f0aa96efa9cedcc685cc6e8b5640cbdd2689da7
jakobhes <38102871+jakobhes@users.noreply.github.com>
2023-03-25 14:59:02 +0100

    added validation and persistence to User class

--- src/main/java/com/example/userapi/User.java
+++ src/main/java/com/example/userapi/User.java
@@ -1,10 +1,40 @@
 package com.example.userapi;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotBlank(message = "Name is mandatory")
     private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @Min(value = 1, message = "Age should not be less than 1")
     private int age;
+
+    public User() {
+        // Default constructor
+    }
+
+    public User(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
 
     // getters and setters
     public long getId() {
@@ -37,5 +67,38 @@ public class User {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    /**
+     * Compares this user to another object for equality.
+     *
+     * @param o The object to compare.
+     * @return true if the objects are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (age != user.age) return false;
+        if (!name.equals(user.name)) return false;
+        return email.equals(user.email);
+    }
+
+    /**
+     * Generates a hash code for this user object.
+     *
+     * @return The hash code value for this object.
+     */
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + name.hashCode();
+        result = 31 * result + email.hashCode();
+        result = 31 * result + age;
+        return result;
     }
 }
